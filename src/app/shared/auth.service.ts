0293feebeae9b0752b79704aeca862b518ff9a69
Alex Kopen <alexkopen@gmail.com>
2018-03-20 14:39:46 -0500

    Added token refresh

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -3,6 +3,10 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import * as auth0 from 'auth0-js';
 import { AUTH_CONFIG } from './auth-config';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/mergeMap';
 
 @Injectable()
 export class AuthService {
@@ -20,6 +24,8 @@ export class AuthService {
   // Create a stream of logged in status to communicate throughout app
   loggedIn: boolean;
   loggedIn$ = new BehaviorSubject<boolean>(this.loggedIn);
+
+  refreshSubscription: any;
 
   constructor(private router: Router) {
     // If authenticated, set local profile property and update login status subject
@@ -71,15 +77,18 @@ export class AuthService {
     });
   }
 
-  private _setSession(authResult, profile) {
+  private _setSession(authResult, profile?) {
     const expTime = authResult.expiresIn * 1000 + Date.now();
     // Save session data and update login status subject
     localStorage.setItem('token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem('profile', JSON.stringify(profile));
     localStorage.setItem('expires_at', JSON.stringify(expTime));
-    this.userProfile = profile;
+    if (profile) {
+      localStorage.setItem('profile', JSON.stringify(profile));
+      this.userProfile = profile;
+    }
     this.setLoggedIn(true);
+    this.scheduleRenewal();
     this.router.navigate(['/dashboard']);
   }
 
@@ -90,6 +99,7 @@ export class AuthService {
     localStorage.removeItem('profile');
     localStorage.removeItem('expires_at');
     this.userProfile = undefined;
+    this.unscheduleRenewal();
     this.setLoggedIn(false);
   }
 
@@ -97,6 +107,51 @@ export class AuthService {
     // Check if current date is greater than expiration
     const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return Date.now() < expiresAt;
+  }
+
+  public scheduleRenewal() {
+    if (!this.authenticated) {
+      return;
+    }
+
+    this.unscheduleRenewal();
+
+    const expiresAt = JSON.parse(window.localStorage.getItem('expires_at'));
+
+    const source = Observable.of(expiresAt).flatMap(
+      expiresAt => {
+
+        const now = Date.now();
+
+        // Use the delay in a timer to
+        // run the refresh at the proper time
+        return Observable.timer(Math.max(1, expiresAt - now));
+      });
+
+    // Once the delay time from above is
+    // reached, get a new JWT and schedule
+    // additional refreshes
+    this.refreshSubscription = source.subscribe(() => {
+      this.renewToken();
+      this.scheduleRenewal();
+    });
+  }
+
+  public unscheduleRenewal() {
+    if (!this.refreshSubscription) {
+      return;
+    }
+    this.refreshSubscription.unsubscribe();
+  }
+
+  public renewToken() {
+    this.auth0.checkSession({}, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this._setSession(result);
+      }
+    });
   }
 
 }
