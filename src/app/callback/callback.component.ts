c5c4937d40ce5fd27a0d9012b2cc4bdd3f3d9f70
AlexKopen <alexkopen@gmail.com>
2018-06-16 18:04:12 -0500

    Changed styling to sass

--- src/app/callback/callback.component.ts
+++ src/app/callback/callback.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-callback',
   templateUrl: './callback.component.html',
-  styleUrls: ['./callback.component.css']
+  styleUrls: ['./callback.component.scss']
 })
 export class CallbackComponent implements OnInit, OnDestroy {
   loggedInSub: Subscription;
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from './../auth/auth.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
   profileArray = this._makeProfileArray(this.auth.userProfile);
