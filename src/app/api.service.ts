c41ee3bc5f8649b43217ee9b418d802e430f4157
AlexKopen <alexkopen@gmail.com>
2018-06-29 11:12:30 -0500

    better error handling

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,16 +11,24 @@ export class ApiService {
 
   constructor(private http: HttpClient, private auth: AuthService) {}
 
-  getDragons$(): Observable<any[]> {
+  getDragons$(): Observable<Dragon[]> {
     return this.http
       .get<Dragon[]>(`${this.baseUrl}dragons`, {
         headers: new HttpHeaders().set('Authorization', `Bearer ${this.auth.accessToken}`)
       })
-      .pipe(catchError(this._handleError));
+      .pipe(catchError(this.handleError));
   }
 
-  private _handleError(err: HttpErrorResponse | any) {
-    const errorMsg = err.message || 'Unable to retrieve data';
-    return throwError(errorMsg);
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
   }
 }
